093c61b1f0ff9f501e070080e0cea408f37e0c0f
ekaya <tremirhankaya@gmail.com>
2026-07-28 14:24:07 +0300

    initialize project structure with Maven, add application configuration and dependencies

---
+++ src/main/java/com/infina/portfoliomanagement/PortfoliomanagementApplication.java
@@ -0,0 +1,13 @@
+package com.infina.portfoliomanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoliomanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfoliomanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infina/portfoliomanagement/PortfoliomanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infina.portfoliomanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoliomanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
