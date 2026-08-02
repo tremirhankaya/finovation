fe7621026aa2710c21a1e83f0f0caa284b0137eb
ekaya <tremirhankaya@gmail.com>
2026-08-02 12:00:10 +0300

    FINOV-76 fix(be): move API key from query param to x-api-key header

--- finovation-be/src/main/java/com/infina/portfoliomanagement/marketdata/config/InfinaClientConfig.java
+++ finovation-be/src/main/java/com/infina/portfoliomanagement/marketdata/config/InfinaClientConfig.java
@@ -3,13 +3,7 @@ package com.infina.portfoliomanagement.marketdata.config;
 import com.infina.portfoliomanagement.marketdata.client.InfinaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.support.HttpRequestWrapper;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
 
 @Configuration
 public class InfinaClientConfig {
@@ -19,24 +13,7 @@ public class InfinaClientConfig {
         return builder
                 .baseUrl(properties.baseUrl())
                 .defaultHeader("Accept", "application/json")
-                .requestInterceptor(apiKeyInterceptor(properties.apiKey()))
+                .defaultHeader("x-api-key", properties.apiKey())
                 .build();
-    }
-
-    private ClientHttpRequestInterceptor apiKeyInterceptor(String apiKey) {
-        return (request, body, execution) -> {
-            URI newUri = UriComponentsBuilder.fromUri(request.getURI())
-                    .queryParam("api_key", apiKey)
-                    .build(true)
-                    .toUri();
-
-            HttpRequest wrapped = new HttpRequestWrapper(request) {
-                @Override
-                public URI getURI() {
-                    return newUri;
-                }
-            };
-            return execution.execute(wrapped, body);
-        };
     }
 }
