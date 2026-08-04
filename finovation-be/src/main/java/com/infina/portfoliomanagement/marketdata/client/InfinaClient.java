a2f8e75a89a942535f4685945a743c8574b4784f
ekaya <tremirhankaya@gmail.com>
2026-08-04 11:03:39 +0300

    FINOV-79 refactor(be): remove old CSV ingest package

--- finovation-be/src/main/java/com/infina/portfoliomanagement/marketdata/client/InfinaClient.java
+++
@@ -1,9 +0,0 @@
-package com.infina.portfoliomanagement.marketdata.client;
-
-import org.springframework.util.MultiValueMap;
-
-import java.util.List;
-
-public interface InfinaClient {
-    <T> List<T> get(String endpoint, MultiValueMap<String, String> params, Class<T> itemType);
-}
