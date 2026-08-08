418ccf68dc7b2765c6894e37b8eb8dda4754657a
Sefa Ecir <sefacir@hotmail.com>
2026-08-08 15:45:00 +0300

    FINOV-93 fix(optimization): clear stale rows before fund_id type change to avoid silent data loss

--- finovation-be/src/main/resources/db/migration/V21__change_optimization_request_fund_id_to_uuid.sql
+++ finovation-be/src/main/resources/db/migration/V21__change_optimization_request_fund_id_to_uuid.sql
@@ -1,3 +1,18 @@
+DELETE FROM dbo.optimization_result_assets
+WHERE result_id IN (SELECT id FROM dbo.optimization_results);
+
+DELETE FROM dbo.optimization_results;
+
+DELETE FROM dbo.optimization_constraint_checks;
+
+DELETE FROM dbo.optimization_asset_limit_overrides;
+
+DELETE FROM dbo.optimization_asset_preferences;
+
+DELETE FROM dbo.optimization_request_constraint_targets;
+
+DELETE FROM dbo.optimization_requests;
+
 IF EXISTS (
     SELECT 1
     FROM sys.indexes
@@ -11,11 +26,7 @@ ALTER TABLE dbo.optimization_requests
     DROP COLUMN fund_id;
 
 ALTER TABLE dbo.optimization_requests
-    ADD fund_id UNIQUEIDENTIFIER NOT NULL
-        CONSTRAINT df_optimization_requests_fund_id DEFAULT NEWID() WITH VALUES;
-
-ALTER TABLE dbo.optimization_requests
-    DROP CONSTRAINT df_optimization_requests_fund_id;
+    ADD fund_id UNIQUEIDENTIFIER NOT NULL;
 
 CREATE INDEX ix_optimization_requests_fund_id
     ON dbo.optimization_requests (fund_id);
