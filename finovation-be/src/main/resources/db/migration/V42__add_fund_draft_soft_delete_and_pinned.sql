2f132ef76fc717950cd52d8b9377741daac5d5a5
ekaya <tremirhankaya@gmail.com>
2026-08-10 21:19:37 +0300

    Fix migration version conflicts

--- finovation-be/src/main/resources/db/migration/V42__add_fund_draft_soft_delete_and_pinned.sql
+++ finovation-be/src/main/resources/db/migration/V42__add_fund_draft_soft_delete_and_pinned.sql
@@ -2,7 +2,10 @@ ALTER TABLE dbo.fund_drafts
     ADD is_deleted  BIT NOT NULL
             CONSTRAINT df_fund_drafts_is_deleted DEFAULT 0,
         design_mode VARCHAR(20) NOT NULL
-            CONSTRAINT df_fund_drafts_design_mode DEFAULT 'AI_ASSISTED';
+            CONSTRAINT df_fund_drafts_design_mode DEFAULT 'AI_ASSISTED',
+        is_pinned BIT NOT NULL
+            CONSTRAINT df_fund_drafts_is_pinned DEFAULT 0,
+        deleted_by_user_id BIGINT NULL;
 GO
 
 ALTER TABLE dbo.fund_drafts
